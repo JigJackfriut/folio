0588519b64007442410a4240287a05fcc0a0ff17
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2026-03-21 00:05:32 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {children}
 
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places`}
-          strategy="afterInteractive"
-        />
+  src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=places&loading=async&v=weekly`}
+  async
+  defer
+/>
       </body>
     </html>
   )
